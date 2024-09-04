3be0c7a95d2c7ecccc65ee5915c5ead3e738bcf7
Juampy Flowers <sorestbug@gmail.com>
2024-09-04 17:43:06 -0600

    Practica 5, Ejercicio 2

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -1,7 +1,7 @@
 <?php
 
 //Ejercicio 1
-echo "<br>";
+echo "<b>Ejercicio 1: </b> <br> <br>";
 $_myvar = "\$_myvar es válida <br> <br>";
 echo $_myvar;
 unset($_myvar);
@@ -14,19 +14,32 @@ unset($_7var);
 echo "myvar NO es válida por no empezar con $ <br> <br>";
 
 $myvar = "\$myvar es válida <br> <br>";
-echo $myvar . "\n";
+echo $myvar;
 unset($myvar);
 
 $var7 = "\$var7 es válida <br> <br>";
-echo $var7 . "\n";
+echo $var7;
 unset($var7);
 
 $_element1 = "\$_element1 es válida <br> <br>";
-echo $_element1 . "\n";
+echo $_element1;
 unset($_element1);
 
 //$house*5
 echo "\$house*5 NO es válida por tener * <br> <br>"; 
 
+//Ejercicio 2 
+echo "<b>Ejercicio 2: </b> <br> <br>";
+$a = "ManejadorSQL";
+$b = 'MySQL';
+$c = &$a;
+echo $a . "<br> <br>";
+echo $b . "<br> <br>";
+echo $c . "<br> <br>";
+$a = "PHP server";
+$b = &$a;
+echo $a . "<br> <br>";
+echo $b . "<br> <br>";
+echo $c . "<br> <br>";
 
 ?>
