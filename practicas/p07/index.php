c362cebbc96166cbb321ad22a77b43e7ef77e7e2
Juampy Flowers <sorestbug@gmail.com>
2024-09-11 17:41:37 -0600

    practica 7 ejercicio 2

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -12,10 +12,8 @@
 
     <?php
     include './src/funciones.php';
-    
     if (isset($_GET['numero'])) { // Verificar si se ha pasado el número como parámetro en la URL
         $numero = $_GET['numero'];
-        
         if (Multiplo5y7($numero)) {
             echo "<p>El número " . htmlspecialchars($numero) . " es múltiplo de 5 y 7.</p>";
         } else {
@@ -25,6 +23,13 @@
         echo "<p>Por favor, proporciona un número en la URL como ?numero=.</p>";
     }
     ?>
+
+    <h2>Ejercicio 2</h2>
+    <h3>Generar 3 números aleatorioa hasta tener impar/par/impar</h3>
+    <?php
+    generarSecuencia();
+    ?>
+
 
 </body>
 </html>
--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -1,5 +1,31 @@
 <?php
-function Multiplo5y7($numero) {
+
+function Multiplo5y7($numero) { //Ejercicio 1
     return $numero % 5 == 0 && $numero % 7 == 0;
 }
+
+function generarSecuencia() { //Ejercicio 2
+    $matriz = array();
+    $iteraciones = 0;
+    $numerosGenerados = 0;
+
+    do {
+        $num1 = rand(1, 999);
+        $num2 = rand(1, 999);
+        $num3 = rand(1, 999);
+
+        $matriz[] = array($num1, $num2, $num3);
+        $iteraciones++;
+        $numerosGenerados += 3;     
+    } while (!($num1 % 2 != 0 && $num2 % 2 == 0 && $num3 % 2 != 0)); // Verificar si la secuencia es impar, par, impar
+
+    
+    echo "<table border='1'>"; // Mostrar la matriz de secuencias generadas
+    foreach ($matriz as $fila) {
+        echo "<tr><td>{$fila[0]}</td><td>{$fila[1]}</td><td>{$fila[2]}</td></tr>";
+    }
+    echo "</table>";
+    echo "<p>$numerosGenerados números obtenidos en $iteraciones iteraciones.</p>";
+}
+
 ?>
