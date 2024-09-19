4eb1136fd27dd7c0b513f96e89f2231136384e10
Juampy Flowers <sorestbug@gmail.com>
2024-09-19 17:53:49 -0600

    actividad XHTML 1

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -1,125 +1,140 @@
 <?php
 
-//Ejercicio 1
-echo "<b>Ejercicio 1: </b> <br> <br>";
-$_myvar = "\$_myvar es válida <br> <br>";
+// Ejercicio 1
+echo "<b>Ejercicio 1: </b><br><br>";
+
+$_myvar = "\$_myvar es válida <br><br>";
 echo $_myvar;
 unset($_myvar);
 
-$_7var = "\$_7var es válida <br> <br>";
+$_7var = "\$_7var es válida <br><br>";
 echo $_7var;
 unset($_7var);
 
 // myvar
-echo "myvar NO es válida por no empezar con $ <br> <br>";
+echo "myvar NO es válida por no empezar con \$ <br><br>";
 
-$myvar = "\$myvar es válida <br> <br>";
+$myvar = "\$myvar es válida <br><br>";
 echo $myvar;
 unset($myvar);
 
-$var7 = "\$var7 es válida <br> <br>";
+$var7 = "\$var7 es válida <br><br>";
 echo $var7;
 unset($var7);
 
-$_element1 = "\$_element1 es válida <br> <br>";
+$_element1 = "\$_element1 es válida <br><br>";
 echo $_element1;
 unset($_element1);
 
-//$house*5
-echo "\$house*5 NO es válida por tener * <br> <br>"; 
+// $house*5
+echo "\$house*5 NO es válida por tener * <br><br>";
 
-//Ejercicio 2 
-echo "<b>Ejercicio 2: </b> <br> <br>";
+// Ejercicio 2
+echo "<b>Ejercicio 2: </b><br><br>";
 $a = "ManejadorSQL";
 $b = 'MySQL';
 $c = &$a;
-echo $a . "<br> <br>";
-echo $b . "<br> <br>";
-echo $c . "<br> <br>";
+
+echo "$a<br><br>";
+echo "$b<br><br>";
+echo "$c<br><br>";
+
 $a = "PHP server";
 $b = &$a;
-echo $a . "<br> <br>";
-echo $b . "<br> <br>";
-echo $c . "<br> <br>";
-unset($a);
-unset($b);
-unset($c);
 
-//Ejercicio 3
-echo "<b>Ejercicio 3: </b> <br> <br>";
+echo "$a<br><br>";
+echo "$b<br><br>";
+echo "$c<br><br>";
+
+unset($a, $b, $c);
+
+// Ejercicio 3
+echo "<b>Ejercicio 3: </b><br><br>";
 $a = "PHP5";
-echo $a . "<br> <br>";
+echo "$a<br><br>";
 
 $z[] = &$a;
 print_r($a);
-echo "<br> <br>";
+echo "<br><br>";
 
 $b = "5a version de PHP";
-echo $b . "<br> <br>";
+echo "$b<br><br>";
 
-$c = $b*10;
-echo $c . "<br> <br>";
+// Asegúrate de extraer un número de $b si es posible
+$c = 0;
+if (preg_match('/(\d+)/', $b, $matches)) {
+    $c = (int)$matches[0] * 10; // Extrae el primer número y multiplica
+}
+echo "$c<br><br>";
 
 $a .= $b;
-echo $a . "<br> <br>";
+echo "$a<br><br>";
 
-$b *= $c;
-echo $b . "<br> <br>";
+$b_numeric = 0;
+if (preg_match('/(\d+)/', $b, $matches)) {
+    $b_numeric = (int)$matches[0]; // Extrae el primer número
+}
+$b_numeric *= $c; // Multiplica solo si se extrajo un número
+echo "$b_numeric<br><br>";
 
 $z[0] = "MySQL";
 print_r($a);
-echo "<br> <br>";
-unset($a);
-unset($b);
-unset($c);
-unset($z);
+echo "<br><br>";
 
-//Ejercicio 4
-echo "<b>Ejercicio 4: </b> <br> <br>";
+unset($a, $b, $c, $z);
+
+
+// Ejercicio 4
+echo "<b>Ejercicio 4: </b><br><br>";
 $GLOBALS['a'] = "PHP5";
-echo $a . "<br> <br>";
+echo $GLOBALS['a'] . "<br><br>";
 
 $GLOBALS['z'][] = &$GLOBALS['a'];
-print_r($a);
-echo "<br> <br>";
+print_r($GLOBALS['a']);
+echo "<br><br>";
 
 $GLOBALS['b'] = "5a version de PHP";
-echo $b . "<br> <br>";
+echo $GLOBALS['b'] . "<br><br>";
 
-$GLOBALS['c'] = $b*10;
-echo $c . "<br> <br>";
+// Asegúrate de extraer un número de $b si es posible
+$GLOBALS['c'] = 0;
+if (preg_match('/(\d+)/', $GLOBALS['b'], $matches)) {
+    $GLOBALS['c'] = (int)$matches[0] * 10; // Extrae el primer número y multiplica
+}
+echo $GLOBALS['c'] . "<br><br>";
 
-$a .= $b;
-echo $a . "<br> <br>";
+$GLOBALS['a'] .= $GLOBALS['b'];
+echo $GLOBALS['a'] . "<br><br>";
 
-$b *= $c;
-echo $b . "<br> <br>";
+$b_numeric = 0;
+if (preg_match('/(\d+)/', $GLOBALS['b'], $matches)) {
+    $b_numeric = (int)$matches[0]; // Extrae el primer número
+}
+$b_numeric *= $GLOBALS['c']; // Multiplica solo si se extrajo un número
+echo "$b_numeric<br><br>";
 
-$z[0] = "MySQL";
-print_r($a);
-echo "<br> <br>";
-unset($a);
-unset($b);
-unset($c);
-unset($z);
+$GLOBALS['z'][0] = "MySQL";
+print_r($GLOBALS['a']);
+echo "<br><br>";
 
-//Ejercicio 5
-echo "<b>Ejercicio 5: </b> <br> <br>";
+unset($GLOBALS['a'], $GLOBALS['b'], $GLOBALS['c'], $GLOBALS['z']);
+
+// Ejercicio 5
+echo "<b>Ejercicio 5: </b><br><br>";
 $a = "7 personas";
-echo $a . "<br> <br>";
+echo "$a<br><br>";
 $b = (integer) $a;
-echo $b . "<br> <br>";
+echo "$b<br><br>";
+
 $a = "9E3";
-echo $a . "<br> <br>";
+echo "$a<br><br>";
 $c = (double) $a;
-echo $c . "<br> <br>";
+echo "$c<br><br>";
 
-unset($a);
-unset($b);
-unset($c);
+unset($a, $b, $c);
 
-//Ejercicio 6
-echo "<b>Ejercicio 6: </b> <br> <br>";
+// Ejercicio 6
+echo "<b>Ejercicio 6: </b><br><br>";
 $a = "0";
 echo "Valor de \$a: ";
 var_dump($a);
@@ -154,26 +169,19 @@ echo "Valor de \$c como cadena: " . var_export($c, true) . "<br>";
 echo "Valor de \$e como cadena: " . var_export($e, true) . "<br>";
 echo "<br>";
 
-unset($a);
-unset($b);
-unset($c);
-unset($d);
-unset($e);
-unset($f);
+unset($a, $b, $c, $d, $e, $f);
 
-//Ejercicio 7
-echo "<b>Ejercicio 7: </b> <br> <br>";
+// Ejercicio 7
+echo "<b>Ejercicio 7: </b><br><br>";
 if (isset($_SERVER['SERVER_SOFTWARE'])) {
     $apache_version = $_SERVER['SERVER_SOFTWARE'];
-    echo "Versión de Apache: " . $apache_version . "<br>";
+    echo "Versión de Apache: $apache_version<br>";
 } else {
     echo "No se puede determinar la versión de Apache.<br>";
 }
 echo "Versión de PHP: " . phpversion() . "<br>";
 
 echo "Sistema operativo del servidor: " . php_uname('s') . "<br>";
-
-echo "Idioma del navegador (cliente): " . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "<br>";
-echo "<br>";
+echo "Idioma del navegador (cliente): " . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "<br><br>";
 
 ?>
