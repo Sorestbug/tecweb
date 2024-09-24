e15583071509fc52d3ace5ed48cdb23ecf2f6f4e
Juampy Flowers <sorestbug@gmail.com>
2024-09-23 19:25:16 -0600

    Practica 9, Ejercicio 2

--- practicas/p09/set_producto.php
+++ practicas/p09/set_producto.php
@@ -1,32 +1,33 @@
 <?php
-$nombre = 'nombre_producto';
-$marca  = 'marca_producto';
-$modelo = 'modelo_producto';
-$precio = 1.0;
-$detalles = 'detalles_producto';
-$unidades = 1;
-$imagen   = 'img/imagen.png';
+// Capturar los datos del formulario enviados por POST
+$nombre = $_POST['nombre'];
+$marca  = $_POST['marca'];
+$modelo = $_POST['modelo'];
+$precio = $_POST['precio'];
+$detalles = $_POST['detalles'];
+$unidades = $_POST['unidades'];
+$imagen =  $_POST['imagen'];
 
-/** SE CREA EL OBJETO DE CONEXION */
-@$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');	
+/** SE CREA EL OBJETO DE CONEXIÓN */
+@$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');
 
-/** comprobar la conexión */
-if ($link->connect_errno) 
-{
-    die('Falló la conexión: '.$link->connect_error.'<br/>');
+/** Comprobar la conexión */
+if ($link->connect_errno) {
+    die('Falló la conexión: ' . $link->connect_error . '<br/>');
     /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 }
 
-/** Crear una tabla que no devuelve un conjunto de resultados */
-$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
-if ( $link->query($sql) ) 
-{
-    echo 'Producto insertado con ID: '.$link->insert_id;
-}
-else
-{
-	echo 'El Producto no pudo ser insertado =(';
+/** Crear la consulta SQL */
+$sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) 
+        VALUES ('{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+
+/** Ejecutar la consulta */
+if ($link->query($sql)) {
+    echo 'Producto insertado con ID: ' . $link->insert_id;
+} else {
+    echo 'El Producto no pudo ser insertado =('; 
 }
 
+/** Cerrar la conexión */
 $link->close();
 ?>
