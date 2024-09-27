f9f47f4e44930b38f0f171f9825de488d3dfb06f
Juampy Flowers <sorestbug@gmail.com>
2024-09-27 12:40:28 -0600

    Actividad JS ejercicio 2y3

--- Actividades/JavaScript para principiantes/src/main.js
+++ Actividades/JavaScript para principiantes/src/main.js
@@ -1,7 +1,31 @@
-
-// Función Ejericicio 1
+//Ejercicios JS:
 function ejercicio1() {
-    let resultado = "Hola Mundo!";
-    document.getElementById("div1").innerHTML = resultado;
+    let mensaje = "Hola Mundo!";
+    document.getElementById("div1").innerHTML = mensaje;
+}
+
+function ejercicio2() {
+    var nombre = 'Juampy';
+    var edad = 25;
+    var altura = 1.68;
+    var casado = false;
+    
+    var resultado = "Nombre: " + nombre + "<br>" +
+                    "Edad: " + edad + "<br>" +
+                    "Altura: " + altura + " metros<br>" +
+                    "Casado: " + (casado ? "Sí" : "No");
+    
+    document.getElementById("div2").innerHTML = resultado;
+}
+
+function ejercicio3() {
+    var nombre;
+    var edad;
+    
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    var mensaje = 'Hola ' + nombre + ', así que tienes ' + edad + ' años.';
+
+    document.getElementById("div3").innerHTML = mensaje;
 }
 
