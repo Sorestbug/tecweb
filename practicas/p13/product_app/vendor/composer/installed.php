f901d5d8096699ca6e37bfe1f66be9277d7336d6
Juampy Flowers <sorestbug@gmail.com>
2024-11-11 18:19:21 -0600

    practica 13 prueba compsoer

---
+++ practicas/p13/product_app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'champ/product_app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '53338456069eaa0d4abba35df95e1532703c9488',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'champ/product_app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '53338456069eaa0d4abba35df95e1532703c9488',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
